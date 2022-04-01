02d8031895a81db6c2f2b1f34340ba547736a34c
isnegirev34@gmail.com <isnegirev34@gmail.com>
2022-04-01 16:41:46 +1000

    3

--- ConAppl/ConAppl.cpp
+++ ConAppl/ConAppl.cpp
@@ -14,43 +14,96 @@ class BlackJeck
     int iSummPoint = 0;
 
 public:
-    bool GetCart(char cCardDeck[])
+    char GetCart(char cCardDeck[])
     {
-        int i = 0;
-
-        do
+        for (int i = 0; i < 56; i++)
         {
-            if (cCardDeck[i] != 0)
-            {
-                cSteck[iCursor++] = cCardDeck[i];
-                return true;
-            }
-            else
-            {
-                i++;
-            }
+            int b = rand () % 55;
 
-        } while (i >= 56);
+            if (cCardDeck[b] != 0 && cCardDeck[b] != '0')
+            {
+                char c;
 
-        return false;
+                cSteck[this->iCursor++] = cCardDeck[b];
+
+                c = cCardDeck[b];
+                
+                cCardDeck[b] = 0;
+
+                return c;
+            }
+        } 
     }
 
     int SummPoint()
     {
-        for (int i = 0; i < iCursor; i++)
-        {
-            this->iSummPoint += this->cSteck[i];
-        }
+        
+            if (cSteck[this->iCursor] == '1')
+            {
+                this->iSummPoint += 1;
+            }
+            else if (cSteck[i] == '2')
+            {
+                this->iSummPoint += 2;
+            }
+            else if (cSteck[i] == '3')
+            {
+                this->iSummPoint += 3;
+            }
+            else if (cSteck[i] == '4')
+            {
+                this->iSummPoint += 4;
+            }
+            else if (cSteck[i] == '5')
+            {
+                this->iSummPoint += 5;
+            }
+            else if (cSteck[i] == '6')
+            {
+                this->iSummPoint += 6;
+            }
+            else if (cSteck[i] == '7')
+            {
+                this->iSummPoint += 7;
+            }
+            else if (cSteck[i] == '8')
+            {
+                this->iSummPoint += 8;
+            }
+            else if (cSteck[i] == '9')
+            {
+                this->iSummPoint += 9;
+            }
+            else if (cSteck[i] == '10')
+            {
+                this->iSummPoint += 10;
+            }
+            else if (cSteck[i] == 'L' || cSteck[i] == 'V'|| cSteck[i] == 'K')
+            {
+                this->iSummPoint += 10;
+            }
+            else if (cSteck[i] == 'A')
+            {
+                this->iSummPoint += 11;
+            }
+        
 
         return this->iSummPoint;
     }
 
-    void SummCash(int iCash)
+    int GetSummPoint()
     {
-        this->iSummCash += iCash;
+        return this->iSummPoint;
     }
 
-    int ReturnCash (int iCash)
+    int SummCash(int &iCash)
+    {
+        this->iSummCash += iCash;
+
+        return iCash = 0;
+    }
+
+    int GetCash (int iCash)
     {
         return iCash = this->iSummCash;
     }
@@ -63,9 +116,13 @@ class Gamer : public BlackJeck
     std::string stName;
 
 public:
-    std::string GetName(std::string stName)
+    void SetName(std::string stName)
     {
         this->stName = stName;
+    }
+
+    std::string GetName()
+    {
         return this->stName;
     }
 };
@@ -86,24 +143,61 @@ int main()
 
     cout << "Введите имя " << endl;
         
-    cin >> stName;
-    g.GetName(stName);
+    //cin >> stName;
+    g.SetName(stName);
 
     cout << "\nВведите начальную ставку " << endl;
 
-    cin >> iCash;
+    //cin >> iCash;
     c.SummCash (iCash);
 
-    g.
+    cout << "Вы взяти карту " << g.GetCart(cCardDeck);
+    cout << "\nВы взяти карту " << g.GetCart(cCardDeck);
+    cout << "\nСумма Ваших очков " << g.SummPoint();
 
+    cout << endl;
 
-    do
+    if (g.GetSummPoint() == 21)
     {
-        
+        cout << g.GetName() << " Выйграл";
 
+        g.GetCash(iCash);
+    }
+    else if (g.GetSummPoint() > 21) // Баг
+    {
+        cout << g.GetName() << " Проиграл";
+    }
+    else
+    {
+        int i = 0;
 
+        do
+        {
+            if (i % 2 == 0)
+            {
+                cout << "\nКрупье взял карту " << c.GetCart(cCardDeck);
 
-    } while (true);
+                cout << "\nСумма очков крупье " << c.SummPoint();
+            }
+            else
+            {
+                cout << "\nВзять карту? (y/n) " << endl;
+                char c;
+                cin >> c;
+
+                if (c == 'y' || c == 'Y')
+                {
+                    cout << "\nВы взяти карту " << g.GetCart(cCardDeck);
+
+                    cout << "\nСумма Ваших очков " << g.SummPoint();
+                }
+            }
+
+            i++;
+
+        } while (g.GetSummPoint() >= c.GetSummPoint() || c.GetSummPoint() >= g.GetSummPoint() 
+                    || c.GetSummPoint() > 21 || g.GetSummPoint() > 21);
+    }
 
     return 0;
 }   
