4657360d70684ce29fcfcfbb9a811dad2c6bdab9
isnegirev34@gmail.com <isnegirev34@gmail.com>
2022-04-01 14:56:54 +1000

    0

--- ConAppl/ConAppl.cpp
+++ ConAppl/ConAppl.cpp
@@ -1,6 +1,5 @@
 ﻿#include <iostream>
 
-
 using namespace std;
 
 //3 Класса
@@ -66,32 +65,6 @@ int main()
 {
     setlocale(LC_ALL, "Ru");
 
-    Power one;
-    one.SetVoltageAndDegree();
-    one.Calculate();
-
-    cout << endl;
-
-    RGBA rgba(5,2,4,8);
-    rgba.print();
-
-    cout << endl;
-
-    Stack stack;
-    stack.reset();
-    stack.print();
-
-    stack.push(3);
-    stack.push(7);
-    stack.push(5);
-    stack.print();
-
-    stack.pop();
-    stack.print();
-
-    stack.pop();
-    stack.pop();
-    stack.print();
 
     return 0;
 }   
