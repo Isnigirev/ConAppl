4898617aaefd0db63a882f203687455fbbe24084
IluaSN <ilua.snegirev.97@mail.ru>
2022-04-03 23:58:18 +1000

    2

--- ConAppl/ConAppl.cpp
+++ ConAppl/ConAppl.cpp
@@ -2,73 +2,132 @@
 
 using namespace std;
 
-/*
-Во всех остальных классах переопределить эту функцию, исходя из геометрических формул нахождения площади.
-*/
+//1
 
 class Figure
 {
-    
+
 public:
     Figure ()
     {
         
     }
 
-    virtual area()
-    {
-
-    }
-};
-
-class Parallelogram : public Figure
-{
-public:
-    Parallelogram()
-    {
-
-    }
-};
-
-class Circle : public Figure
-{
-public:
-    Circle ()
+    virtual void area()
     {
         
     }
 };
 
+class Parallelogram : public Figure
+{
+    float fLength = 0; // длина 
+    float fHeight = 0; // высоты
+
+public:
+    Parallelogram (float fLength = 0, float fHeight = 0)
+    {
+        this->fLength = fLength;
+        this->fHeight = fHeight;
+    }
+
+    virtual void area()
+    {
+        std::cout << "S Parallelogram = " << fLength * fHeight << std::endl;
+    }
+};
+
+class Circle : public Figure
+{
+    const float fPI = 3.14;
+    float fRadius;              //радиус
+
+public:
+    Circle (float fLength)
+    {
+        fRadius = fLength / 2;
+    }
+
+    virtual void area()
+    {
+        std:: cout << "S Circle = " << fPI * (fRadius * fRadius) << std::endl;
+    }
+};
+
 class Rectangle: public Parallelogram
 {
-public:
-    Rectangle()
-    {
+    float fLength = 0;
+    float fWidth = 0;
 
+public:
+    Rectangle(float fLength = 0, float fWidth = 0)
+    {
+        this->fLength = fLength;
+        this->fWidth = fWidth;
+    }
+
+    virtual void area()
+    {
+        std::cout << "S Rectangle = " << fLength * fWidth << std::endl;
     }
 };
 
 class Square : public Parallelogram
 {
-public:
-    Square()
-    {
+    float fSide = 0;
 
+public:
+    Square(float fSide = 0)
+    {
+        this->fSide = fSide;
+    }
+
+    virtual void area()
+    {
+        std::cout << "S Square = " << fSide * fSide << std::endl;
     }
 };
 
 class Rhombus : public Parallelogram
 {
-public:
-    Rhombus()
-    {
+    float fDiagonalA = 0;
+    float fDiagonalB = 0;
 
+public:
+    Rhombus(float fDiagonalA = 0, float fDiagonalB = 0)
+    {
+        this->fDiagonalA = fDiagonalA;
+        this->fDiagonalB = fDiagonalB;
+    }
+
+    virtual void area()
+    {
+        std::cout << "S Rhombus = " << (fDiagonalA * fDiagonalB) / 2 << std::endl;
     }
 };
+
+//2
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
+
+    //1
+
+    Circle c (5);
+    Parallelogram p(3, 4);
+    Rectangle rec(2, 5);
+    Square s(4);
+    Rhombus rh(4, 4);
+
+    c.area();
+    p.area();
+    rec.area();
+    s.area();
+    rh.area();
+
+    //2
+
 
 
     return 0;
