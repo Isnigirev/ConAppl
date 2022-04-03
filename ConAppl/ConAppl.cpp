af72dc22c89ad10d559dea16590b48fd81cb4838
IluaSN <ilua.snegirev.97@mail.ru>
2022-04-03 22:11:59 +1000

    3/1

--- ConAppl/ConAppl.cpp
+++ ConAppl/ConAppl.cpp
@@ -2,37 +2,69 @@
 
 using namespace std;
 
+/*
+Во всех остальных классах переопределить эту функцию, исходя из геометрических формул нахождения площади.
+*/
+
 class Figure
 {
+    
+public:
+    Figure ()
+    {
+        
+    }
 
+    virtual area()
+    {
+
+    }
 };
 
 class Parallelogram : public Figure
 {
+public:
+    Parallelogram()
+    {
 
+    }
 };
 
 class Circle : public Figure
 {
-
+public:
+    Circle ()
+    {
+        
+    }
 };
 
 class Rectangle: public Parallelogram
 {
+public:
+    Rectangle()
+    {
 
+    }
 };
 
 class Square : public Parallelogram
 {
+public:
+    Square()
+    {
 
+    }
 };
 
 class Rhombus : public Parallelogram
 {
+public:
+    Rhombus()
+    {
 
+    }
 };
-
-
 
 int main()
 {
